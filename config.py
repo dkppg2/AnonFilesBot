17703a0710578f363c7602d47bb54e5197460a51
tenshi-cs <116143418+tenshi-cs@users.noreply.github.com>
2022-10-19 16:37:36 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,10 +2,10 @@ import os
 
 
 class Config(object):
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "5682981728:AAEbr-VF2ZMwlmyoe-pFlsPBZE4XYmMY5E8")
+    BOT_TOKEN = "5682981728:AAEbr-VF2ZMwlmyoe-pFlsPBZE4XYmMY5E8"
 
-    APP_ID = int(os.environ.get("APP_ID", 10247139))
+    APP_ID = 10247139
 
-    API_HASH = os.environ.get("API_HASH", "96b46175824223a33737657ab943fd6a")
+    API_HASH = "96b46175824223a33737657ab943fd6a"
 
-    UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "-1001763515187")
+    UPDATES_CHANNEL = "-1001763515187"
